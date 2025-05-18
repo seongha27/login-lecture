e263e389a46422bdc279fc1f252e2f7009329a54
성하의 맥 <seongha@seonghauiui-MacBookAir.local>
2025-05-18 21:49:11 +0900

    프런트 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,41 @@
 "use strict";
 
-const hello = (req, res) => {
-  res.render("home/index");
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
-const login = (req, res) => {
-  res.render("home/login");
+const users = {
+  id: ["sungha", "성하박사", "성하"],
+  password: ["123", "1234", "12345"],
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      password = req.body.password;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.password[idx] === password) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
+  },
 };
 
 module.exports = {
-  hello,
-  login,
+  output,
+  process,
 };
